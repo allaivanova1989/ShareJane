823efccfb5234b98be4de93ccc7ee0bc8b176d42
allaivanova1989 <baby20086@rambler.ru>
2021-11-14 18:46:10 +0300

    ty7ugty7

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -21,29 +21,35 @@ public class SignUpTest {
         WebElement continueButton = driver.findElement(By.cssSelector("[value=Continue]"));
         continueButton.click();
         // Убедиться что мы настранице SignUp1
-        boolean isDisplayed = driver.findElement(By.cssSelector("[value=Register]")).isDisplayed();
-        Assert.assertTrue(isDisplayed, "Oops, we have a bug");
-        // Закрыть браузер
-        driver.quit();
+        try {
+            boolean isDisplayed = driver.findElement(By.cssSelector("[value=Register]")).isDisplayed();
+        } catch (Exception e) {
+            Assert.assertTrue(false, "We have a bug. Error message has not been displayed.");
+        } finally {
+            driver.quit();
+        }
     }
 
     @Test
     public void ZipCodeShouldAccept6Digits() {
         // Открытие страницы https://www.sharelane.com/cgi-bin/register.py
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver2 = new ChromeDriver();
-        driver2.get("https://www.sharelane.com/cgi-bin/register.py");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
         // Ввести шесть цифр
-        WebElement zipCodeInput2 = driver2.findElement(By.name("zip_code"));
-        zipCodeInput2.sendKeys("123456");
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("123456");
         // нажимаем кнопку continue
-        WebElement continueButton2 = driver2.findElement(By.cssSelector("[value=Continue]"));
-        continueButton2.click();
+        WebElement continueButton = driver.findElement(By.cssSelector("[value=Continue]"));
+        continueButton.click();
         // Убедиться что есть сообщение об ошибке
-        boolean isDisplayed2 = driver2.findElement(By.cssSelector(".error_message")).isDisplayed();
-        Assert.assertTrue(isDisplayed2, "Oops, we have a bug");
-        // Закрыть браузер
-        driver2.quit();
+        try {
+            boolean isDisplayed = driver.findElement(By.cssSelector(".error_message")).isDisplayed();
+        } catch (Exception e) {
+            Assert.assertTrue(false, "We have a bug. Error message has not been displayed.");
+        } finally {
+            driver.quit();
+        }
 
     }
 
@@ -51,19 +57,22 @@ public class SignUpTest {
     public void ZipCodeShouldAccept4Digits() {
         // Открытие страницы https://www.sharelane.com/cgi-bin/register.py
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver3 = new ChromeDriver();
-        driver3.get("https://www.sharelane.com/cgi-bin/register.py");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
         // Ввести четыре цифр
-        WebElement zipCodeInput3 = driver3.findElement(By.name("zip_code"));
-        zipCodeInput3.sendKeys("1234");
+        WebElement zipCodeInput = driver.findElement(By.name("zip_code"));
+        zipCodeInput.sendKeys("1234");
         // нажимаем кнопку continue
-        WebElement continueButton3 = driver3.findElement(By.cssSelector("[value=Continue]"));
-        continueButton3.click();
+        WebElement continueButton = driver.findElement(By.cssSelector("[value=Continue]"));
+        continueButton.click();
         // Убедиться что есть сообщение об ошибке
-        boolean isDisplayed3 = driver3.findElement(By.cssSelector(".error_message")).isDisplayed();
-        Assert.assertTrue(isDisplayed3, "Oops, we have a bug");
-        // Закрыть браузер
-        driver3.quit();
+        try {
+            boolean isDisplayed3 = driver.findElement(By.cssSelector(".error_message")).isDisplayed();
+        } catch (Exception e) {
+            Assert.assertTrue(false, "We have a bug. Error message has not been displayed.");
+        } finally {
+            driver.quit();
+        }
     }
 
 }
